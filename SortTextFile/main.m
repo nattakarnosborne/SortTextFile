2f90b9461cfd484e12b739c6771daf0bff851ed1
nanybay <nattakarnosborne@nattakarns-imac.home>
2015-11-12 08:40:40 -0500

    Initial Commit

---
+++ SortTextFile/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SortTextFile
+//
+//  Created by Nattakarn Osborne on 11/12/15.
+//  Copyright © 2015 Nattakarn Osborne. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
